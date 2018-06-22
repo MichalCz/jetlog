1ce18a66bac47c3d8cea65d956ab48b9424e2364
michal@vantage <cz@signicode.com>
2018-06-22 14:15:46 +0200

    Reporter and filtering

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const JetLog = require("./lib/jetlog")
-const defaultLogger = new JetLog();
+const JetLog = require("./lib/jetlog");
+const {testEnv, getEnv} = require("./lib/env-default");
 
+const defaultLogger = new JetLog()
+    .use(getEnv('JETLOG_REPORTER', './lib/entry-reporter'))
+    .catch(e => `Invalid log entry in ${e && e.stack && e.stack.split('\n')[1]}`);
+defaultLogger.JetLog = JetLog;
+
+if (testEnv("JETLOG", true)) {
+    defaultLogger
+        .pipe(process.stdout);
+}
+
+if (testEnv("JETLOG_FILTER")) {
+    const filters = getEnv("JETLOG_FILTER").split(/\s+/);
+    defaultLogger
+        .filter(
+            ({text}) => filters.find(needle => text.indexOf(needle) >= 0)
+        );
+}
+
+module.exports = defaultLogger;
